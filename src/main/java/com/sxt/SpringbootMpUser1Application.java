fbe5baf84400f3ad66d1399fbc65e3a9cc16f39f
雷哥 <78414842@qq.com>
2019-04-24 10:05:59 +0800

    首次提交

---
+++ src/main/java/com/sxt/SpringbootMpUser1Application.java
@@ -0,0 +1,13 @@
+package com.sxt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMpUser1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMpUser1Application.class, args);
+	}
+
+}
